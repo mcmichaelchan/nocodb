28d26e634c8da973f7aaf4b5ba3c073b0f688c40
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-09-15 14:17:08 +0200

    feat(nc-gui): add stepper to survey page

--- packages/nc-gui/components.d.ts
+++ packages/nc-gui/components.d.ts
@@ -134,6 +134,7 @@ declare module '@vue/runtime-core' {
     MdiChevronDoubleLeft: typeof import('~icons/mdi/chevron-double-left')['default']
     MdiChevronDown: typeof import('~icons/mdi/chevron-down')['default']
     MdiChevronLeft: typeof import('~icons/mdi/chevron-left')['default']
+    MdiChevronRight: typeof import('~icons/mdi/chevron-right')['default']
     MdiClose: typeof import('~icons/mdi/close')['default']
     MdiCloseBox: typeof import('~icons/mdi/close-box')['default']
     MdiCloseCircle: typeof import('~icons/mdi/close-circle')['default']
