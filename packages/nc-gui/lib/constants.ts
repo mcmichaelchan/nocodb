1bcd5c3398b9b127bda73c31445a69f802bf47a2
mcjjchen <mcjjchen@tencent.com>
2022-10-25 14:40:26 +0800

    ci: add debug info

--- packages/nc-gui/lib/constants.ts
+++ packages/nc-gui/lib/constants.ts
@@ -4,6 +4,7 @@ export const NOCO = 'noco'
 
 export const SYSTEM_COLUMNS = ['id', 'title', 'created_at', 'updated_at']
 export const BASE_URL = process.env.NC_BACKEND_URL || (process.env.NODE_ENV === 'production' ? '..' : 'http://localhost:8090')
+console.log(BASE_URL, process.env)
 
 /**
  * Each permission value means the following
