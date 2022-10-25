e05d6b637982c415f892252744a85ea09f152398
mcjjchen <mcjjchen@tencent.com>
2022-10-25 16:26:37 +0800

    fix: fix backend url of prod env

--- packages/nc-gui/lib/constants.ts
+++ packages/nc-gui/lib/constants.ts
@@ -3,8 +3,8 @@ import { ProjectRole, Role } from './enums'
 export const NOCO = 'noco'
 
 export const SYSTEM_COLUMNS = ['id', 'title', 'created_at', 'updated_at']
-export const BASE_URL = process.env.NC_BACKEND_URL || (process.env.NODE_ENV === 'production' ? '..' : 'http://localhost:8090')
-console.log(BASE_URL, process.env)
+export const BASE_URL =
+  process.env.NC_BACKEND_URL || (process.env.NODE_ENV === 'production' ? '../nocoapi' : 'http://localhost:8090')
 
 /**
  * Each permission value means the following
